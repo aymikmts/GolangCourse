c233cf46ae89988180da67024f4f5c5dabf48ff0
aymikmts <rohy.2nd@gmail.com>
2017-03-26 21:00:36 +0900

    Add ch02/ex01 code and test.

---
+++ ch02/ex01/tempconv/conv.go
@@ -0,0 +1,30 @@
+package tempconv
+
+import "math"
+
+// pointは四捨五入する小数点桁数
+const point int = 3
+
+// CToFは摂氏の温度を華氏へ変換します。
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(round(float64(c)*9/5+32, point)) }
+
+// CToKは摂氏の温度を絶対温度へ変換します。
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
+// FToCは華氏の温度を摂氏へ変換します。
+func FToC(f Fahrenheit) Celsius { return Celsius(round((float64(f)-32)*5/9, point)) }
+
+// FToKは華氏の温度を絶対温度へ変換します。
+func FToK(f Fahrenheit) Kelvin { return Kelvin(CToK(FToC(f))) }
+
+// KToCは絶対温度を摂氏へ変換します。
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// KToFは絶対温度を華氏へ変換します。
+func KToF(k Kelvin) Fahrenheit { return Fahrenheit(CToF(KToC(k))) }
+
+// roundはfを小数点以下places桁で四捨五入します。
+func round(f float64, places int) float64 {
+	shift := math.Pow(10, float64(places))
+	return math.Floor(f*shift+.5) / shift
+}
---
+++ ch02/ex01/tempconv/tempconv.go
@@ -0,0 +1,19 @@
+// tempconvパッケージは摂氏(Celsius)と華氏(Fahrenheit)の温度変換を行います
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func (c Celsius) String() string    { return fmt.Sprintf("%g℃", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g℉", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%gK", k) }
+
