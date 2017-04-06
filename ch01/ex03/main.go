81403364c9b61a21b1afcf2e28b70cf13b1594ca
aymikmts <rohy.2nd@gmail.com>
2017-04-06 13:08:24 +0900

    Modify measuring the time of Echo2 and Echo3.

--- ch01/ex03/main.go
+++ ch01/ex03/main.go
@@ -2,13 +2,23 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	"./echo2"
 	"./echo3"
 )
 
 func main() {
+	start := time.Now()
 	echo2.Echo(os.Args[1:])
+	ms := time.Since(start).Nanoseconds()
+	fmt.Printf("echo2 time: %vs\n\n", ms)
+
+	start = time.Now()
 	echo3.Echo(os.Args[1:])
+	ms = time.Since(start).Nanoseconds()
+	fmt.Printf("echo3 time: %vs\n", ms)
+
 }
