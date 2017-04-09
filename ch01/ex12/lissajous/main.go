3cf02040a7de755fd446c3fdcefc2cda47ca9af8
aymikmts <rohy.2nd@gmail.com>
2017-04-09 17:55:24 +0900

    Add ch01/ex12 code.

---
+++ ch01/ex12/lissajous/main.go
@@ -0,0 +1,70 @@
+// lissagousはURLからパラメータ値を読み取り、周回の回数を設定してリサージュ曲線をlocalhost:8000に描画します。
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.Black}
+var cycles float64 = 5
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	for key, values := range r.URL.Query() {
+		if key == "cycles" {
+			param, err := strconv.Atoi(values[0])
+			if err != nil {
+				fmt.Fprintf(w, "invalid value:%v\n", values[0])
+			}
+			cycles = float64(param)
+		}
+	}
+	lissajous(w)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		//cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
---
+++ ch01/ex12/runall.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd `dirname $0`
+
+#go run lissajous/main.go &
+#open http://localhost:8000/?cycles=20
+
