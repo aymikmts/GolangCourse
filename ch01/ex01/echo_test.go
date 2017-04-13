3510e53d871fef1060faff40f2c240be0877e81d
aymikmts <rohy.2nd@gmail.com>
2017-04-13 12:31:50 +0900

    Add test code of ch01/ex01.

---
+++ ch01/ex01/echo_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestEcho(t *testing.T) {
+	var tests = []struct {
+		args []string
+		want string
+	}{
+		{[]string{"test1 test2 test3"}, "test1 test2 test3\n"},
+		{[]string{""}, "\n"},
+	}
+
+	for _, test := range tests {
+		descr := fmt.Sprintf("echo(%v)", test.args)
+
+		out = new(bytes.Buffer)
+		if err := echo(test.args); err != nil {
+			t.Errorf("%s failed; %v", descr, err)
+			continue
+		}
+		got := out.(*bytes.Buffer).String()
+		ret := strings.Contains(got, test.want)
+		if !ret {
+			t.Errorf("%s = %q, want %q", descr, got, test.want)
+		}
+	}
+}
--- ch01/ex01/main.go
+++ ch01/ex01/main.go
@@ -3,14 +3,25 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
+var out io.Writer = os.Stdout
+
 func main() {
+	if err := echo(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "echo: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func echo(args []string) error {
 	s, sep := "", ""
-	for _, arg := range os.Args[:] {
+	for _, arg := range args[:] {
 		s += sep + arg
 		sep = " "
 	}
-	fmt.Println(s)
+	fmt.Fprintln(out, s)
+	return nil
 }
