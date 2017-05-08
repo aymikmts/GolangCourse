37ce492acf3409a68d6e428e1e2fe2a680d87286
aymikmts <rohy.2nd@gmail.com>
2017-05-08 21:57:36 +0900

    Modified changing how to input to standard input from command arguments.

--- ch04/ex03/main.go
+++ ch04/ex03/main.go
@@ -1,39 +1,45 @@
 // Ex03はint配列を直接逆順に並び替えます。
 // 配列の大きさは"6"に固定です。
-// 配列はコマンド引数より生成します。
+// 配列は標準入力より入力された数値から生成します。
 package main
 
 import (
 	//"GolangCourse/ch04/rev""
 
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 
 	"../rev"
 )
 
 func main() {
+
 	// 配列の作成
-	var array [6]int
-	for i, v := range os.Args {
-		if i == 0 {
-			continue
-		}
-		if i > len(array) {
-			break
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		var array [6]int
+		s := strings.Fields(input.Text())
+		for i, v := range s {
+			if i >= len(array) {
+				break
+			}
+
+			val, err := strconv.Atoi(v)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "v = %v, Atoi error: %v\n", v, err)
+				os.Exit(1)
+			}
+			array[i] = val
 		}
 
-		val, err := strconv.Atoi(v)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "v = %v, Atoi error: %v\n", v, err)
-			os.Exit(1)
-		}
-		array[i-1] = val
+		fmt.Printf("input : %v\n", array)
+
+		// Ex03の実行
+		rev.ReverseByPointer(&array)
+
+		fmt.Printf("output: %v\n", array)
 	}
-
-	fmt.Printf("input : %v\n", array)
-
-	rev.ReverseByPointer(&array)
-	fmt.Printf("output: %v\n", array)
 }
--- ch04/ex03/runall.bash
+++ ch04/ex03/runall.bash
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd `dirname $0`
 
-go run main.go 0 1 2 3 4 5
-go run main.go 10 20 30
-go run main.go test1 test2 test3 test4 test5
+go run main.go < input
