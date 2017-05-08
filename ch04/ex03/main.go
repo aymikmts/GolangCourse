9e0e5815086e4841aa9b5fba9d8951118254b431
aymikmts <rohy.2nd@gmail.com>
2017-05-08 21:27:43 +0900

    Modified how to input value to array.

--- ch04/ex03/main.go
+++ ch04/ex03/main.go
@@ -1,21 +1,39 @@
 // Ex03はint配列を直接逆順に並び替えます。
 // 配列の大きさは"6"に固定です。
+// 配列はコマンド引数より生成します。
 package main
 
 import (
 	//"GolangCourse/ch04/rev""
 
 	"fmt"
+	"os"
+	"strconv"
 
 	"../rev"
 )
 
 func main() {
-	a := [6]int{0, 1, 2, 3, 4, 5}
-	rev.ReverseByPointer(&a)
-	fmt.Println(a)
+	// 配列の作成
+	var array [6]int
+	for i, v := range os.Args {
+		if i == 0 {
+			continue
+		}
+		if i > len(array) {
+			break
+		}
 
-	b := [6]int{1, 2, 3}
-	rev.ReverseByPointer(&b)
-	fmt.Println(b)
+		val, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "v = %v, Atoi error: %v\n", v, err)
+			os.Exit(1)
+		}
+		array[i-1] = val
+	}
+
+	fmt.Printf("input : %v\n", array)
+
+	rev.ReverseByPointer(&array)
+	fmt.Printf("output: %v\n", array)
 }
--- ch04/ex03/runall.bash
+++ ch04/ex03/runall.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd `dirname $0`
 
-go run main.go
+go run main.go 0 1 2 3 4 5
+go run main.go 10 20 30
+go run main.go test1 test2 test3 test4 test5
