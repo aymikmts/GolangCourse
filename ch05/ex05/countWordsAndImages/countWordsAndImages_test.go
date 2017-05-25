ba7cdc548089e91d1b208004f457f976df648278
aymikmts <rohy.2nd@gmail.com>
2017-05-26 08:07:46 +0900

    Add ch05/ex05 code and test.

---
+++ ch05/ex05/countWordsAndImages/countWordsAndImages_test.go
@@ -0,0 +1,48 @@
+package countwordsandimages
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"testing"
+
+	"golang.org/x/net/html"
+)
+
+func TestCountWordsAndImages(t *testing.T) {
+
+	var tests = []struct {
+		testData string
+		words    int
+		images   int
+	}{
+		{"test.html", 10, 2},
+	}
+
+	for _, test := range tests {
+		n := readTestFile(test.testData)
+		w, i := countWordsAndImages(nil, n)
+		if w != test.words {
+			t.Errorf("words is %d, but got is %d.", test.words, w)
+		}
+		if i != test.images {
+			t.Errorf("images is %d, but got is %d.", test.images, i)
+		}
+	}
+}
+
+// test用HTMLファイルを読み込む
+func readTestFile(fname string) *html.Node {
+	file, err := os.Open(fname)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot open %s, %v\n", fname, err)
+	}
+	input := io.Reader(file)
+
+	doc, err := html.Parse(input)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	return doc
+}
--- ch05/ex05/main.go
+++ ch05/ex05/main.go
@@ -1,87 +1,25 @@
-// Ex05 は
+// Ex05 は標準入力から得たurlをGETリクエストし、
+// そのドキュメント内に含まれる単語と画像の数を返します。
 package main
 
 import (
+	"GolangCourse/ch05/ex05/countWordsAndImages"
+	"bufio"
 	"fmt"
 	"os"
-	"strings"
-
-	"bufio"
-
-	"golang.org/x/net/html"
 )
 
 func main() {
-	// input := bufio.NewScanner(os.Stdin)
-	// for input.Scan() {
-	// 	url := input.Text()
-	url := ""
-	words, images, err := CountWordsAndImages(url)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ex05: %v\n", err)
-		os.Exit(1)
-	}
+	input := bufio.NewScanner(os.Stdin)
 
-	fmt.Printf("words: %d\timages:%d\n", words, images)
-	//}
-}
-
-// CountWordsAndImagesはHTMLドキュメントに対するHTTP GETリクエストをurlへ
-// 行い、そのドキュメント内に含まれる単語と画像の数を返します。
-func CountWordsAndImages(url string) (words, images int, err error) {
-	// resp, err := http.Get(url)
-	// if err != nil {
-	// 	return
-	// }
-	// doc, err := html.Parse(resp.Body)
-	// resp.Body.Close()
-	// if err != nil {
-	// 	err = fmt.Errorf("parsing HTML: %s", err)
-	// 	return
-	// }
-
-	doc, err := html.Parse(os.Stdin)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ex05: %v\n", err)
-		os.Exit(1)
-	}
-	words, images = countWordsAndImages(doc)
-	return
-}
-
-// countWordsAndImagesは単語と画像の数を返します。
-func countWordsAndImages(n *html.Node) (words, images int) {
-	var stack []string
-	switch n.Type {
-	case html.ElementNode:
-		stack = append(stack, n.Data) // push tag
-		if stack[len(stack)-1] == "img" {
-			images++
-		}
-	case html.TextNode:
-		length := len(stack)
-		if length > 0 {
-			if stack[length-1] != "script" && stack[length-1] != "style" {
-				scanner := bufio.NewScanner(strings.NewReader(n.Data))
-				words += countWords(scanner)
-			}
-		}
-
-	}
-
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		w, i := countWordsAndImages(c)
-		words += w
-		images += i
-	}
-	return
-}
-
-func countWords(input *bufio.Scanner) int {
-	var counts int
-	input.Split(bufio.ScanWords)
 	for input.Scan() {
-		counts++
+		url := input.Text()
+		words, images, err := countwordsandimages.CountWordsAndImages(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ex05: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("[%s]\nwords: %d\timages:%d\n", url, words, images)
 	}
-	return counts
 }
