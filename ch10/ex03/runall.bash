fc5c82a8a8bbddba44f2016561c95ee9e2fc090e
aymikmts <rohy.2nd@gmail.com>
2017-07-30 17:23:16 +0900

    Add ch10/ex03 result.

---
+++ ch10/ex03/runall.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd `dirname $0`
+
+go build -o fetch fetch.go
+./fetch http://gopl.io/ch1/helloworld?go-get=1
+
+# <!DOCTYPE html>
+# <html>
+# <head>
+# <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+# <meta name="go-import" content="gopl.io git https://github.com/adonovan/gopl.io">
+# </head>
+# <body>
+# </body>
+# </html>
